43f6281cabd782eb8088a230cfff7e74d0d0c7f5
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-07-13 01:53:23 +0530

    structure,loading,slug,fonts,layout

---
+++ src/app/contact/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>Loading</div>//Loading animation between page switching
+  )
+}
+
+export default Loading
---
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
+// (auth) is group folder so it will not take it as path in url directory

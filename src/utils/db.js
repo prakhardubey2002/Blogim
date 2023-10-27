643f6361c82f7c11cf0fe3283fcb70711debdfbe
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-10-27 22:08:53 +0530

    cluster created,Moongoose version degraded to 6.11.1,Schema created,env added

---
+++ src/utils/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+    } catch (error) {
+        throw new Error(` Connection Failed : ${error}`);
+    }
+}

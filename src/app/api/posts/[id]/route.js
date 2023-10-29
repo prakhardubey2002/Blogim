0fed60069b6a3c333aaf53addea88e93987fe9a8
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-10-30 01:23:40 +0530

    Project completed,Dashboard completed,api endpoints added,

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -15,3 +15,18 @@ export const GET = async (request,{params})=>{
     }
 
 }
+export const DELETE = async (request,{params})=>{
+    const {id}=params
+    //fetch
+    try{
+        await connect();
+        await Post.findByIdAndDelete(id)
+        
+
+        return new NextResponse(`Post has been deleted `,{status:200});
+    }catch(err){
+        return new NextResponse(`Database Error ${err} `,{status:500});
+        
+    }
+
+}

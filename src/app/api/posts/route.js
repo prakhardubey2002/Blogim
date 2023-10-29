f9c91390f82590db5a9b9ad8eefe9b0c2297f6fa
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-10-30 01:23:40 +0530

    Project completed,Dashboard completed,api endpoints added,

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -7,8 +7,22 @@ export const GET = async (request)=>{
     //fetch
     try{
         await connect();
-        const posts=await Post.find( username && {username} )//if user exist specific post is returned else whole content is returned
+        const posts=await Post.find( username && {username} )
+        //if user exist specific post is returned else whole content is returned
         return new NextResponse(JSON.stringify(posts),{status:200});
+
+    }catch(err){
+        
+        return new NextResponse(`Database Error ${err} `,{status:500});
+    }
+
+}
+export const POST = async (request)=>{
+   const body=await request.json();
+   const newPost=new Post(body)
+    try{
+        await connect();
+        await newPost.save();
 
     }catch(err){
         
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,7 @@ import useSWR from 'swr';
 import styles from "./page.module.css"
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 const Dashboard = () => {
   // const [data, setData] = useState([]);
   // const [err, setErr] = useState(false);
@@ -28,7 +29,7 @@ const Dashboard = () => {
   //For normal RESTful APIs with JSON data, first you need to create a fetcher function, which is just a wrapper of the native fetch:
   const session = useSession();
   const fetcher = (...args) => fetch(...args).then(res => res.json())
-  const { data, err, isLoading } = useSWR(`/api/posts?username=${session?.data?.user.name}`, fetcher)
+  const { data,mutate, err, isLoading } = useSWR(`/api/posts?username=${session?.data?.user.name}`, fetcher)
   console.log(data)
   //Stale-While-Revalidate It is highly recommended if you are fetching data on the client-side. It handles caching, revalidation, focus tracking, refetching on intervals, and more
   // console.log(data);
@@ -39,19 +40,68 @@ const Dashboard = () => {
   if (session.status == "unauthenticated") {
     router?.push("/dashboard/login");
   }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+    try {
+      await fetch("/api/posts",{
+        method:"POST",
+        body:JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username:session?.data?.user?.name,
+        })
+      })
+      mutate();// mutate is given swr so we dont have to refresh page when data is updated using form here
+    } catch (err) {
+     console.log(err);
 
+    }
+  }
+ 
 
   console.log(session)
+  const handleDelete=async(id)=>{
+    try{
+      await fetch(`api/posts/${id}`,{
+        method:"DELETE",
+      });
+      mutate();// mutate is given swr so we dont have to refresh page when data is updated using form here
+    }catch(err){
+      console.log(err);
+    }
+  }
   return (
     <div className={styles.container} >
-      Dashboard
-      {/* <div className='q' >
-        {!isLoading && (data?.map((item) => (
-          <div key={item.id} > {item.title}</div>
-        )
-        )
-        )}
-      </div> */}
+      <div className={styles.posts} >
+        { isLoading ? "Loading " : data?.map(post => (
+          <div className={styles.post} key={post._id} >
+            <div className={styles.imgConatiner} >
+              <Image src={post.img} width={200} height={200} alt="" />
+            </div>
+            <h2 className={styles.postTitle} >{post.title }</h2>
+            <span onClick={()=>handleDelete(post._id)} className={styles.delete} >X</span>
+          </div>
+        ))}
+      </div>
+      <form className={styles.new} onSubmit={handleSubmit} >
+        <h1>Add New Post</h1>
+        <input type="text" placeholder='Title' className={styles.input} />
+        <input type="text" placeholder='Desc' className={styles.input} />
+        <input type="text" placeholder='Image' className={styles.input} />
+        <textarea
+          placeholder='Content'
+          className={styles.textArea}
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button className={styles.button} >Send</button>
+      </form>
     </div>
   )
 }
